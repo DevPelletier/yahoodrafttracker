ecd7d4ec8be40cfe2c19c53c1e9b43d729a086f9
DevPelletier <devonpelletier1@gmail.com>
2023-09-17 21:04:35 -0400

    test

--- app/page.js
+++ app/page.js
@@ -10,10 +10,10 @@ import { useSearchParams } from 'next/navigation'
 export default function Home() {
   
   const searchParams = useSearchParams()
-  const token = searchParams.get('token')
-  console.log(token)
+  const accessCode = searchParams.get('code')
+  console.log(accessCode)
 
-  const hostname = "localhost:3000"
+  const hostname = "https://yahoodrafttracker.vercel.app"
   const client_id = "dj0yJmk9V0hLeGZhdHUzdXVBJmQ9WVdrOVRsVm9aSEYzY3pRbWNHbzlNQT09JnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWFj";
   const redirect_uri = `${hostname}/test`;
   // const redirect_uri = "localhost:3000"; // as a backup if ngrok stops working for whatever dumb reason lol
--- app/test/page.js
+++ app/test/page.js
@@ -4,8 +4,8 @@ import { useSearchParams } from 'next/navigation'
 
 export default function Page() {
     const searchParams = useSearchParams()
-    const token = searchParams.get('token')
-    console.log(token)
+    const access_code = searchParams.get('code')
+    console.log(access_code)
 
     return (
     <main>
